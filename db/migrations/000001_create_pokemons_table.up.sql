9ffe7084ef67521582a8cd2c7ea7dce470aa852a
Rafael Koch Peres <rafaelkperes@gmail.com>
2024-05-15 18:54:41 -0300

    Add first migration for pokemons table

---
+++ db/migrations/000001_create_pokemons_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE pokemons (
+  id INTEGER PRIMARY KEY,
+  identifier TEXT,
+  species_id INTEGER,
+  height INTEGER,
+  weight INTEGER,
+  base_experience INTEGER,
+  order INTEGER,
+  is_default INTEGER 
+);
