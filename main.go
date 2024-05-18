2a0ebeafe21f6207552efe62414e0780400ff11e
Rafael Koch Peres <rafaelkperes@gmail.com>
2024-05-18 17:00:30 -0300

    Add preseeded database and remove migrations

--- main.go
+++ main.go
@@ -1,22 +1,4 @@
 package main
 
-import (
-	"log"
-
-	migrate "github.com/golang-migrate/migrate/v4"
-	_ "github.com/golang-migrate/migrate/v4/database/sqlite3"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
-)
-
 func main() {
-	m, err := migrate.New(
-		"file://db/migration",
-		"sqlite3://db/pokitalo.db",
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := m.Up(); err != nil {
-		log.Fatal(err)
-	}
 }
